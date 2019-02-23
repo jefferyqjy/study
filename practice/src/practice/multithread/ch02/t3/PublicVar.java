3249d9b0c0847b0f6ef6c06bb03f0b2afb9c6a23
jefferyqjy <435542925@qq.com>
2019-02-24 02:16:59 +0800

    2.1.5

---
+++ practice/src/practice/multithread/ch02/t3/PublicVar.java
@@ -0,0 +1,23 @@
+package practice.multithread.ch02.t3;
+
+public class PublicVar {
+
+	public String username = "A";
+	public String password = "AA";
+	
+	synchronized public void setValue(String username, String password) {
+		try {
+			this.username = username;
+			Thread.sleep(5000);
+			this.password = password;
+			System.out.println("setValue method thread name = " + Thread.currentThread().getName() + " username = " + username + " password = " + password);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	synchronized public void getValue() {
+		System.out.println("getValue method thread name = " + Thread.currentThread().getName() + " username = " + username + " password = " + password);
+	}
+}
