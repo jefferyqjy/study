52132ca05ecbdba6cef2df5b31b62da02164c903
jefferyqjy <435542925@qq.com>
2019-02-24 17:46:30 +0800

    2.2.3

---
+++ practice/src/practice/multithread/ch02/section02/t6/MyThread1.java
@@ -0,0 +1,20 @@
+package practice.multithread.ch02.section02.t6;
+
+public class MyThread1 extends Thread {
+	
+	private Task task;
+
+	public MyThread1(Task task) {
+		super();
+		this.task = task;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		CommonUtils.beginTime1 = System.currentTimeMillis();
+		task.doLongTimeTask();
+		CommonUtils.endTime1 = System.currentTimeMillis();
+	}
+	
+}
