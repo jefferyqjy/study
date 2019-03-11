1dfa028a1d34fabea0c439d9b33cc098b6d7cfd3
jefferyqjy <435542925@qq.com>
2019-03-11 18:09:19 +0800

    3.1.4

---
+++ practice/src/practice/multithread/ch03/notifyHoldLock/ThreadA.java
@@ -0,0 +1,21 @@
+package practice.multithread.ch03.notifyHoldLock;
+
+/**
+ * @Description
+ * @Author qjy13114
+ * @Date 2019/3/11 17:45
+ */
+public class ThreadA extends Thread {
+
+    private Object lock;
+
+    public ThreadA(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        Service service = new Service();
+        service.testMethod(lock);
+    }
+}
