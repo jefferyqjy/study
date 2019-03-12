e7e4fa5e898ed22edf648e35c1dc310460683d4e
jefferyqjy <435542925@qq.com>
2019-03-13 00:47:47 +0800

    3.1.10

---
+++ practice/src/practice/multithread/ch03/waitOld/Add.java
@@ -0,0 +1,22 @@
+package practice.multithread.ch03.waitOld;
+
+/**
+ * @Description
+ * @Author jefferyqjy
+ * @Date 2019/3/13 0:32
+ */
+public class Add {
+
+    private String lock;
+
+    public Add(String lock) {
+        this.lock = lock;
+    }
+
+    public void add(){
+        synchronized (lock) {
+            ValueObject.list.add("anyString");
+            lock.notifyAll();
+        }
+    }
+}
---
+++ practice/src/practice/multithread/ch03/waitOld/ValueObject.java
@@ -0,0 +1,14 @@
+package practice.multithread.ch03.waitOld;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author jefferyqjy
+ * @Date 2019/3/13 0:31
+ */
+public class ValueObject {
+
+    public static List list = new ArrayList();
+}
