e3c494b25f72a6448fd531153027064d2ad3ab2f
jefferyqjy <435542925@qq.com>
2019-03-13 20:52:24 +0800

    3.1.10-1

---
+++ practice/src/practice/multithread/ch03/p_c_allWait/ValueObject.java
@@ -0,0 +1,11 @@
+package practice.multithread.ch03.p_c_allWait;
+
+/**
+ * @Description
+ * @Author qjy13114
+ * @Date 2019/3/13 19:57
+ */
+public class ValueObject {
+
+    public static String value = "";
+}
---
+++ practice/src/practice/multithread/ch03/p_r_test/ValueObject.java
@@ -0,0 +1,11 @@
+package practice.multithread.ch03.p_r_test;
+
+/**
+ * @Description
+ * @Author qjy13114
+ * @Date 2019/3/13 17:22
+ */
+public class ValueObject {
+
+    public static String value = "";
+}
