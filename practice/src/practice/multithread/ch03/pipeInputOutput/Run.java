4cef67c18e620b877840bae51e88fad9d4e39f2f
jefferyqjy <435542925@qq.com>
2019-03-15 01:21:07 +0800

    3.1.12

---
+++ practice/src/practice/multithread/ch03/pipeInputOutput/Run.java
@@ -0,0 +1,38 @@
+package practice.multithread.ch03.pipeInputOutput;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+/**
+ * @Description
+ * @Author jefferyqjy
+ * @Date 2019/3/15 1:02
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        try {
+            WriteData writeData = new WriteData();
+            ReadData readData = new ReadData();
+
+            PipedInputStream inputStream = new PipedInputStream();
+            PipedOutputStream outputStream = new PipedOutputStream();
+
+            //inputStream.connect(outputStream);
+            outputStream.connect(inputStream);
+
+            ThreadRead threadRead = new ThreadRead(readData, inputStream);
+            threadRead.start();
+
+            Thread.sleep(2000);
+
+            ThreadWrite threadWrite = new ThreadWrite(writeData, outputStream);
+            threadWrite.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
