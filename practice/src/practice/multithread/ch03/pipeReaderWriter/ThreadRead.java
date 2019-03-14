ba5fd0eae06fea06685f3b630c9e4827127be9a9
jefferyqjy <435542925@qq.com>
2019-03-15 01:40:19 +0800

    3.1.13

---
+++ practice/src/practice/multithread/ch03/pipeReaderWriter/ThreadRead.java
@@ -0,0 +1,25 @@
+package practice.multithread.ch03.pipeReaderWriter;
+
+import java.io.PipedReader;
+
+/**
+ * @Description
+ * @Author jefferyqjy
+ * @Date 2019/3/15 1:32
+ */
+public class ThreadRead extends Thread {
+
+    private ReadData read;
+
+    private PipedReader input;
+
+    public ThreadRead(ReadData read, PipedReader input) {
+        this.read = read;
+        this.input = input;
+    }
+
+    @Override
+    public void run() {
+        read.readMethod(input);
+    }
+}
---
+++ practice/src/practice/multithread/ch03/pipeReaderWriter/ThreadWrite.java
@@ -0,0 +1,25 @@
+package practice.multithread.ch03.pipeReaderWriter;
+
+import java.io.PipedWriter;
+
+/**
+ * @Description
+ * @Author jefferyqjy
+ * @Date 2019/3/15 1:31
+ */
+public class ThreadWrite extends Thread {
+
+    private WriteData write;
+
+    private PipedWriter out;
+
+    public ThreadWrite(WriteData write, PipedWriter out) {
+        this.write = write;
+        this.out = out;
+    }
+
+    @Override
+    public void run() {
+        write.writeMethod(out);
+    }
+}
