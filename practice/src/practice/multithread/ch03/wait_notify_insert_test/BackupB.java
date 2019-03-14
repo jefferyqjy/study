c05270fe925e72c46441b9feafc806fbd71caaf3
jefferyqjy <435542925@qq.com>
2019-03-15 01:49:48 +0800

    3.1.14

---
+++ practice/src/practice/multithread/ch03/wait_notify_insert_test/BackupB.java
@@ -0,0 +1,20 @@
+package practice.multithread.ch03.wait_notify_insert_test;
+
+/**
+ * @Description
+ * @Author jefferyqjy
+ * @Date 2019/3/15 1:47
+ */
+public class BackupB extends Thread {
+
+    private DBTools dbTools;
+
+    public BackupB(DBTools dbTools) {
+        this.dbTools = dbTools;
+    }
+
+    @Override
+    public void run() {
+        dbTools.backupB();
+    }
+}
