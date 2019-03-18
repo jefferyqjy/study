114cd3b0ecc7509f2c951ea3e4a8d58e4b91daf7
jefferyqjy <435542925@qq.com>
2019-03-19 02:19:23 +0800

    4.1.16

---
+++ practice/src/practice/multithread/ch04/section1/condition123/F.java
@@ -0,0 +1,12 @@
+package practice.multithread.ch04.section1.condition123;
+
+/**
+ * @Description
+ * @Author jefferyqjy
+ * @Date 2019/3/19 1:44
+ */
+public class F {
+
+    volatile public static int nextPrintWho = 1;
+
+}
