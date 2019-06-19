903233a4be37b798442b56e01889307ced57b7d9
jefferyqjy <435542925@qq.com>
2019-06-19 18:11:12 +0800

    init demo-easyexcel

---
+++ demo-easyexcel/src/main/java/com/example/demo/easyexcel/DemoEasyexcelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEasyexcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEasyexcelApplication.class, args);
+    }
+
+}
--- practice/src/main/java/javademo/java8/thread/ThreadB.java
+++
@@ -1,9 +0,0 @@
-package javademo.java8.thread;
-
-public class ThreadB extends Thread{
-
-	@Override
-	public void run() {
-		System.out.println("thread b is running...");
-	}
-}
